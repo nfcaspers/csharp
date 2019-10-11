aa545c64fd9c14076c5fe6e10810d00727bdf6bf
nfcaspers <noelcaspers04@gmail.com>
2019-10-11 19:22:53 +0200

    Added datatypes

---
+++ datatypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataTypeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: {0}", sizeof(int));
+        }
+    }
+}
