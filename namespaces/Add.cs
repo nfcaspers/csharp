932f7e8a5d6c2fc10e2407d2f6e55d0cca7d486c
nfcaspers <noelcaspers04@gmail.com>
2019-10-12 15:40:39 +0200

    Added namespaces project

---
+++ namespaces/Add.cs
@@ -0,0 +1,10 @@
+namespace AddOperation 
+{
+    public class Adder 
+    {
+        public static int add(int x, int y) 
+        {
+            return x + y;
+        }
+    }
+}
---
+++ namespaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AddOperation;
+
+namespace namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int result = Adder.add(100, 200);
+            Console.WriteLine("Result is {0}", result);
+        }
+    }
+}
