4c03c6b23c216c028c0305136936bf05d2536c1c
nfcaspers <noelcaspers04@gmail.com>
2019-10-26 14:53:26 +0200

    added loop project

---
+++ loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("First loop finished.");
+
+            int n = 0;
+            while (n < 10) 
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            Console.WriteLine("Second loop finished.");
+
+            int c = 0;
+            do {
+                Console.WriteLine(c);
+                c++;
+            } while (c > 10);
+            Console.WriteLine("Third loop finished.");
+
+            string[] x = new string[] {"Hello", ",", "world", "!\n"};
+            foreach (string element in x) {
+                Console.Write(element);
+            }
+            Console.WriteLine("Fourth loop finished");
+        }
+    }
+}
