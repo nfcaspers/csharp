8b52c164293a7be6902ddbb3dfcbcc0e04187905
nfcaspers <noelcaspers04@gmail.com>
2019-10-27 18:14:05 +0100

    Added class project

---
+++ class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace classApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rect1 = new Rectangle();
+            Rectangle Rect2 = new Rectangle();
+
+            Rect1.length = 10.5;
+            Rect1.width = 5.5;
+
+            Rect2.length = 11;
+            Rect2.width = 6;
+
+            Console.WriteLine("Can Rect1 hold Rect2: {0}", Rect1.can_hold(Rect2));
+            Console.WriteLine("Can Rect2 hold Rect1: {0}", Rect2.can_hold(Rect1));
+
+
+        }
+    }
+    class Rectangle 
+    {
+        public double length;
+        public double width;
+        public double area() {
+            return this.length * this.width;
+        }
+        public bool can_hold(Rectangle rect) 
+        {
+            if (this.area() >= rect.area())
+            {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
